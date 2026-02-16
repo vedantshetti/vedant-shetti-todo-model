2c823b5f8d5308474fd1fcdd1115fd6516c6d14f
vedantshetti <vedantshetti123456@gmail.com>
2026-02-16 14:41:45 +0530

    added the four apis for the crud purpose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -2,7 +2,7 @@ const pool = require("../config/db");
 
 exports.getTodos= async (req,res)=>{
     try{
-        const result = await pool.query("select * from todos order by sequence asc, id asc ");
+        const result = await pool.query("select * from todos order by id asc ");
         res.json(result.rows);
 
     } catch(err){
@@ -12,3 +12,39 @@ exports.getTodos= async (req,res)=>{
 
 };
 
+exports.addTodo = async (req,res)=>{
+    try{
+
+        const {title,description}=req.body;
+        const result = await pool.query( `insert into todos (title,description) values ($1,$2) returning *`,[title,description || null]);
+        res.json(result.rows[0]);
+
+    }catch(err){
+        res.status(500).json(err.message);
+    }
+}
+
+exports.updateTodo = async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const {title,description,status} =req.body;
+
+        const result = await pool.query(`update todos set title = coalesce($1,title),description = coalesce($2,description),status = coalesce($3,status) where id = $4 returning *` , [title,description, status,id]);
+        res.json(result.rows[0]);
+    }catch(err){
+        res.status(500).json(err.message);
+    }
+};
+
+exports.deleteTodo = async (req,res)=>{
+    try{
+
+        const{id} = req.params;
+
+        await pool.query("delete from todos where id = $1",[id]);
+        res.json("deleted");
+        
+    }catch(err){
+        res.status(500).json(err.message);
+    }
+};  
